7e230affc23c6fdf877e35bf2b9a9e57ecbdc391
your name here <johnobrien8642@gmail.com>
2020-08-20 13:05:00 -0400

    min_min_phase_2 O(n)? (making question marks because will review exercises against solutions)

--- execution_time_diff.rb
+++ execution_time_diff.rb
@@ -1,6 +1,6 @@
 require 'byebug'
 
-def my_min(arr)
+def my_min_phase_1(arr)
     min = arr[0]
     arr.each do |el1|
         arr.each do |el2|
@@ -9,3 +9,12 @@ def my_min(arr)
     end
     min
 end
+
+def my_min_phase_2(arr)
+    min = arr[0]
+    arr.each { |el| min = el if el < min }
+    min
+end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# puts my_min_phase_2(list)  # =>  -5
